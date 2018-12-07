39570d95f1bb01383cde631f767203c3690d847a
Andoped <1196341787@qq.com>
2018-12-07 09:18:16 +0800

    修改001

---
+++ WebApplication1/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string GetString()
+        {
+            return "你好,MVC！";
+        }
+        public Customer getCustomer()
+        {
+            Customer ct = new Customer();
+            ct.CustomerNmae = "002";
+            ct.Address = "003";
+            return ct;
+        }
+
+
+
+    }
+}
